3b265a8f0f2242097c20dbb0c7219e8ef0fbb024
Utkarsh Mehta <umtebiz@gmail.com>
2020-04-27 05:06:25 +0530

    fetching fonts to the application

--- Recipe/App.js
+++ Recipe/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 export default function App() {
+
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+      'roboto-light': require('./assets/fonts/Roboto-Light.ttf'),
+      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+      'roboto-italic': require('./assets/fonts/Roboto-MediumItalic.ttf'),
+    })
+  }
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
