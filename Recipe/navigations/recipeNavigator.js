62ddf90974da8eef264c4f600538045275656c31
Utkarsh Mehta <umtebiz@gmail.com>
2020-04-28 00:34:59 +0530

    added tab navigation

--- Recipe/navigations/recipeNavigator.js
+++ Recipe/navigations/recipeNavigator.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import CategoryScreen from '../screens/CategoryScreen'
 import SingleCategoryScreen from '../screens/SingleCategoryScreen'
 import SingleRecipeScreen from '../screens/SingleRecipeScreen'
 import CustomHeaderButton from '../components/CustomHeaderButton'
+import FavoriteScreen from '../screens/FavoriteScreen'
 
 const RecipeNavigator = createStackNavigator({
     Category: CategoryScreen,
@@ -24,4 +26,9 @@ const RecipeNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(RecipeNavigator)
+const RecipeTabNavigator = createBottomTabNavigator({
+    Recipes: RecipeNavigator,
+    Favorites: FavoriteScreen,
+})
+
+export default createAppContainer(RecipeTabNavigator)
